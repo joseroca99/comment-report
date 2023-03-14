731dce0ee31b34151a5475cfb17d370e5c02c047
Jose <joselego129@gmail.com>
2023-03-14 15:45:41 -0500

    incorporate cloud

--- qdrant_for_amazon.py
+++ qdrant_for_amazon.py
@@ -3,12 +3,21 @@ import pandas as pd
 import cohere
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import PointStruct
+from pathlib import Path
 
 # Create Cohere client
 co = cohere.Client("In30uIWlPuaZKU3hIaE0vjka8u0Vvd0MyPmgItAD")
 
 # Create Qdrant client
-client = QdrantClient("localhost", port=6333)
+BASE_DIR = Path(__file__).parent.resolve()
+KEY_DIR = BASE_DIR / 'etc' / 'my_key.txt'
+with open(KEY_DIR) as f:
+    SECRET_KEY = f.read().strip()
+
+qdrant_client = QdrantClient(
+    url="https://f8748584-23d6-497d-8831-1265fbb636ff.us-east-1-0.aws.cloud.qdrant.io:6333", 
+    api_key=SECRET_KEY,
+)
 
 # Load dataframe
 df_eng = pd.read_csv("results.csv", names=['title','comment','rating'])
@@ -40,7 +49,7 @@ for index, row in df_eng.iterrows():
         PointStruct(id=index, vector=df.loc[index,'vector'], payload = {'language':df.loc[index,'language']})
     )
 
-# operation_info = client.upsert(
+# operation_info = qdrant_client.upsert(
 #     collection_name="animal",
 #     wait=True,
 #     points=points,
@@ -49,7 +58,7 @@ for index, row in df_eng.iterrows():
 searcher = input('search term: ')
 vector_search = co.embed(texts=[searcher,], model="multilingual-22-12").embeddings[0]
 
-search_result = client.search(
+search_result = qdrant_client.search(
     collection_name="animal",
     query_vector=vector_search, 
     limit=5,
